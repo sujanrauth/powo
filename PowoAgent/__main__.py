eddaae855c807fb6d14f5ac3d45f770d7475a03b
Sujan Rauth Subramanyam <44674236+sujanrauth@users.noreply.github.com>
2025-06-11 12:49:41 -0400

    Delete PowoAgent directory

--- PowoAgent/__main__.py
+++
@@ -1,6 +0,0 @@
-from agent import POWOAgent
-from ichatbio.server import run_agent_server
-
-if __name__ == "__main__":
-    agent = POWOAgent()
-    run_agent_server(agent, host="0.0.0.0", port=9999)
