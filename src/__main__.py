850110e83fb4a28cc1a9cc138b996997a649aa9a
Sujan Rauth Subramanyam <44674236+sujanrauth@users.noreply.github.com>
2025-08-05 17:16:00 -0400

    Update __main__.py
    changed the port to 8989 for listening inside the container

--- src/__main__.py
+++ src/__main__.py
@@ -3,4 +3,4 @@ from ichatbio.server import run_agent_server
 
 if __name__ == "__main__":
     agent = POWOAgent()
-    run_agent_server(agent, host="0.0.0.0", port=9999)
+    run_agent_server(agent, host="0.0.0.0", port=8989)
